7e6f168b980b79bc15aa6c9115d99b4db6e23585
mariasevilla <masegar.25@gmail.com>
2018-03-23 19:01:36 +0100

    ejercicio 1 de la practica

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -8,9 +8,12 @@ conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json?&limit=1", None, headers)
 
 info = conn.getresponse()
+counter= 0
+#for i in info["results"]["0"]:
+    #counter +=1
+    #print("El id es:", i["results"]["id"])
+    #print(i)
 
-for i in info:
-    print(i)
-print(info)
+print(info["results"]["0"])
 #print("El medicamento", informacion['results']['0']['id'])
 conn.close()
