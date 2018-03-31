cb0c032e5acc8f01794f760d436070fea6925012
mariasevilla <masegar.25@gmail.com>
2018-03-31 21:28:23 +0200

    programa3

--- openfda-2/programa3.py
+++ openfda-2/programa3.py
@@ -5,7 +5,7 @@ headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
 
-conn.request("GET", "/drug/label.json?search=active_ingredient:%22acetylsalicylic+acid%22&limit=100", None, headers)
+conn.request("GET", "/drug/label.json?search=active_ingredient:%22acetylsalicylic+acid%22&limit=100&skip", None, headers)
 
 info = conn.getresponse()
 
