aa096c2015f93f69607cc17a50f23e9dafd20cdb
mariasevilla <masegar.25@gmail.com>
2018-04-06 17:30:53 +0200

    ejercicio 1 de la practica

--- openfda-3/servidor.py
+++ openfda-3/servidor.py
@@ -1,57 +1,60 @@
 import socket
+import http.client
+import json
 
 
-IP = "192.168.1.58"
-PORT = 8008
+IP = "212.128.255.132"
+PORT = 8086
 MAX_OPEN_REQUESTS = 10
 
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+
+conn.request("GET", "/drug/label.json?&limit=11", None, headers)
+
+info = conn.getresponse()
+
+print(info.status, info.reason)
+drugs_raw = info.read().decode("utf-8")
+conn.close()
+
+drugs = json.loads(drugs_raw)
 
 def process_client(clientsocket):
 
     mensaje_solicitud = clientsocket.recv(1024)
 
-    headers = {'User-Agent': 'http-client'}
 
-    conn = http.client.HTTPSConnection("api.fda.gov")
 
-    conn.request("GET", "/drug/label.json?&limit=10", None, headers)
 
-    info = conn.getresponse()
 
-    print(info.status, info.reason)
-    drugs_raw = info.read().decode("utf-8")
-    conn.close()
 
-    drugs = json.loads(drugs_raw)
+    contenido = """
+      <!doctype html>
+      <html>
+      <body style='background-color: lightblue'>
+        <h1>Informacion sobre medicamentos</h2>
+        <p></p>
+    """
+
 
     num = 0
-    while num < 10:
-        drug = drugs['results'][num]['openfda']
-        name= drug['generic_name']
+    for drug in drugs['results']:
+
         num += 1
+        if drug['openfda']:
+            contenido += drug['openfda']['generic_name'][0]
+            contenido += """</br></body></html>"""
+
+    linea_inicial = "HTTP/1.1 200 OK\n"
+    cabecera = "Content-Type: text/html\n"
+    cabecera += "Content-Length: {}\n".format(len(str.encode(contenido)))
 
 
-
-        contenido = """
-          <!doctype html>
-          <html>
-          <body style='background-color: lightblue'>
-            <h1>Informacion sobre medicamentos</h2>
-            <p></p>
-          </body>
-          </html>
-        """
-
-
-
-        linea_inicial = "HTTP/1.1 200 OK\n"
-        cabecera = "Content-Type: text/html\n"
-        cabecera += "Content-Length: {}\n".format(len(str.encode(contenido)))
-
-
-        mensaje_respuesta = str.encode(linea_inicial + cabecera + "\n" + contenido + name)
-        clientsocket.send(mensaje_respuesta)
-        clientsocket.close()
+    mensaje_respuesta = str.encode(linea_inicial + cabecera + "\n" + contenido)
+    clientsocket.send(mensaje_respuesta)
+    clientsocket.close()
 
 
 
