3338940bd4e5aa095699d813e8845f9036d26a94
mariasevilla <masegar.25@gmail.com>
2018-04-23 16:21:51 +0200

    practica final

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import request
+import socket
+import http.client
+import json
 
 app = Flask(__name__)
 
@@ -11,17 +14,44 @@ def jsontohtml():
             <h1>Informacion sobre medicamentos</h2>
             <p></p>
         """
-    contenido += "HOLA" #datos(datos sacados de la apirest)
+    contenido += datos() #datos(datos sacados de la apirest)
     contenido += """</br></body></html>"""
 
     return contenido
+
+def datos():
+
+    headers = {'User-Agent': 'http-client'}
+
+    conn = http.client.HTTPSConnection("api.fda.gov")
+
+    conn.request("GET", "/drug/label.json?&limit=10", None, headers)
+
+    info = conn.getresponse()
+
+    print(info.status, info.reason)
+    drugs_raw = info.read().decode("utf-8")
+    conn.close()
+
+    drugs = json.loads(drugs_raw)
+
+    num = 0
+    while num < 10:
+        drug = drugs['results'][num]
+        datos = drug['id']
+        num += 1
+
+
+
+
+
 
 
 @app.route("/listDrug")
 def get_listdrug():
 
     #limite= request.args.get('limit')
-    return jsontohtml(contenido)
+    return jsontohtml()
 
 
 @app.route("/listCompanies")
@@ -43,4 +73,4 @@ def search_company():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8081)
+    app.run(host='0.0.0.0',port=8008)
