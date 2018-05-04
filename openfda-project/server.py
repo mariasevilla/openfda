1c5c3e770ddca4d850e35ec22e5b9e154ba79184
mariasevilla <masegar.25@gmail.com>
2018-05-04 19:12:31 +0200

    practica final

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -6,46 +6,6 @@ import json
 
 app = Flask(__name__)
 
-def jsontohtml():
-    contenido = """
-          <!doctype html>
-          <html>
-          <body style='background-color: lightblue'>
-            <h1>Informacion sobre medicamentos</h2>
-            <p></p>
-        """
-    contenido += datos() #datos(datos sacados de la apirest)
-    contenido += """</br></body></html>"""
-
-    return contenido
-
-def datos():
-
-    headers = {'User-Agent': 'http-client'}
-
-    conn = http.client.HTTPSConnection("api.fda.gov")
-
-    conn.request("GET", "/drug/label.json?&limit=10", None, headers)
-
-    info = conn.getresponse()
-
-    print(info.status, info.reason)
-    drugs_raw = info.read().decode("utf-8")
-    conn.close()
-
-    drugs = json.loads(drugs_raw)
-
-    num = 0
-    while num < 10:
-        drug = drugs['results'][num]
-        datos = drug['id']
-        num += 1
-
-
-
-
-
-
 
 @app.route("/listDrug")
 def get_listdrug():
@@ -60,16 +20,59 @@ def get_listcomp():
 
 @app.route("/searchDrugs")
 def search_drug():
-    ingrediente = request.args.get('active_ingredient')
-    return "search_drug" + ingrediente
+
+    ingrediente = request.args.get('active_ingredient').replace(" ","%20")
+    resultado =  datos("/drug/label.json?search=active_ingredient:"+ingrediente+"&limit=10")
+    mi_html= jsontohtml(resultado)
+    return mi_html
 
 @app.route("/searchCompany")
 def search_company():
     nombre = request.args.get('company')
     return "searchcompany" + nombre
 
+def datos(resultado):
+
+    headers = {'User-Agent': 'http-client'}
+    conn = http.client.HTTPSConnection("api.fda.gov")
+    #conn.request("GET", "/drug/label.json?&limit="+limite, None, headers)
+    conn.request("GET",resultado, None, headers)
+
+    info = conn.getresponse()
+
+    print(info.status, info.reason)
+    drugs_raw = info.read().decode("utf-8")
+    conn.close()
+
+    drugs = json.loads(drugs_raw)
+
+    num = 0
+    info=""
+    if "results" in drugs:
+        for drug in drugs['results']:
+            num +=1
+            if 'generic_name' in drug['openfda']:
+                info += str(drug['openfda']['generic_name'])
+                info += "</br>"
+            else:
+                info+="No tenemos información"
+                info += "</br>"
+    return info
 
 
+
+def jsontohtml(info):
+    mensaje = """
+          <!doctype html>
+          <html>
+          <body style='background-color: lightblue'>
+            <h1>Informacion sobre medicamentos</h2>
+            <p></p>
+        """
+    mensaje += info #datos(datos sacados de la apirest)
+    mensaje += """</br></body></html>"""
+
+    return mensaje
 
 
 if __name__ == "__main__":
